0a610e22c460cd001678ac693bb52c84920d2a28
Sylvee Lee <6053282+hellosylvee@users.noreply.github.com>
2017-11-09 15:49:10 -0500

    Creating a new jQuery project

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+var category = ["restaurants", "groceries", "rent/mortgage", "auto", "entertainment", "savings", "clothing", "medical", "miscellaneous"];
+var total = 0;
+
+$(document).ready(function(){
+  var entryNumber = 0;
+
+  $('button').on('click', function(){
+    console.log('Description is: ' + description + ' and total is: $' + total);
+
+    var description = $('#descriptionInput').val();
+    var amount = $('#amountInput').val();
+    var category = $('#categoryMenu').val();
+    var removeBtn;
+
+    entryNumber += 1;
+    console.log('what is this? ' + removeBtn);
+    $('#table > tbody:last-child').prepend(
+      '<tr><td>' + entryNumber + '</td><td>' + description + '</td><td>' + category + '</td><td>' + amount + '</td></tr>' );
+    $('#descriptionInput').val('');
+    $('#amountInput').val('');
+    total += parseFloat(amount)
+    $('.totalAmount').html(total);
+    console.log('Description is: ' + description + ' and new total is: $' + total);
+    console.log('Category is: ' + category);
+  });
+
+  console.log("hello");
+});
