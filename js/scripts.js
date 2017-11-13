a6a370f63920815289ab9bcbf76b8d04f748b006
Sylvee Lee <6053282+hellosylvee@users.noreply.github.com>
2017-11-13 09:43:13 -0500

    Added delete function for entries

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,29 @@ $(document).ready(function(){
   var entryNumber = 0;
 
   $('button').on('click', function(){
-    console.log('Description is: ' + description + ' and total is: $' + total);
+    // console.log('Description is: ' + description + ' and total is: $' + total);
 
+    var date = document.querySelector('input[type="date"]');
     var description = $('#descriptionInput').val();
     var amount = $('#amountInput').val();
     var category = $('#categoryMenu').val();
-    var removeBtn;
 
+    // console.log('this is the date: ' + date.value);
     entryNumber += 1;
-    console.log('what is this? ' + removeBtn);
+
     $('#table > tbody:last-child').prepend(
-      '<tr><td>' + entryNumber + '</td><td>' + description + '</td><td>' + category + '</td><td>' + amount + '</td></tr>' );
+      '<tr><td>' + entryNumber + '</td><td>' + date.value + '</td><td>' + description + '</td><td>' + category + '</td><td>' + amount + '</td><td><button class="removeEntryBtn">Delete</button></td></tr>' );
     $('#descriptionInput').val('');
     $('#amountInput').val('');
     total += parseFloat(amount)
     $('.totalAmount').html(total);
-    console.log('Description is: ' + description + ' and new total is: $' + total);
-    console.log('Category is: ' + category);
+
+    // console.log('Description is: ' + description + ' and new total is: $' + total);
+    // console.log('Category is: ' + category);
+  });
+
+  $('#table').on('click', 'button', function(){
+    $(this).closest ('tr').remove ();
   });
 
   console.log("hello");
